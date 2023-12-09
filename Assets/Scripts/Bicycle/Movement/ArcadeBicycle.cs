71a380e2fe534d1828ce941233cb35782bfbadd5
Léo Laffont <leo.laffont@etu.univ-smb.fr>
2023-12-09 08:30:47 +0100

    Statisfying results

---
+++ Assets/Scripts/Bicycle/Movement/ArcadeBicycle.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class ArcadeBicycle : MonoBehaviour
+{
+    [SerializeField] GameObject handleBar;
+    [SerializeField] GameObject bicycleVisuals;
+    [SerializeField] float maxWheelTurningAngle;
+    [SerializeField] float wheelTurningSpeed;
+    [SerializeField] float maxBicycleTurningAngle;
+    [SerializeField] float bicycleTurningSpeed;
+    [SerializeField] float accelForce;
+    [SerializeField] float maxSpeed;
+
+
+    Rigidbody rb;
+
+    float horizontalInput = 0;
+    float verticalInput = 0;
+    float currentAccel = 0;
+    float currentVelocity = 0;
+
+    float currentWheelAngle = 0;
+    float currentBicycleAngle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.identity;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // No use of deltaTime since we want arcade physic (so not realistic)
+        currentAccel = Mathf.Clamp(
+            accelForce * verticalInput, 
+            0, 
+            accelForce
+        );
+
+        // Use of deltaTime so bicycle's velocity changes are smooth
+        currentVelocity = Mathf.Clamp(currentVelocity * 0.8f + currentAccel * maxSpeed * Time.fixedDeltaTime, -maxSpeed, maxSpeed);
+
+        // Add force to move forward
+        transform.position += transform.forward * currentVelocity;
+
+        currentWheelAngle = Mathf.Clamp(
+             currentWheelAngle * 0.8f + maxWheelTurningAngle * horizontalInput * Time.fixedDeltaTime * wheelTurningSpeed,
+            -maxWheelTurningAngle,
+            maxWheelTurningAngle
+        );
+
+        currentBicycleAngle = Mathf.Clamp(
+            currentBicycleAngle * 0.8f + maxBicycleTurningAngle * horizontalInput * Time.fixedDeltaTime * bicycleTurningSpeed,
+            -maxBicycleTurningAngle,
+            maxBicycleTurningAngle
+        );
+
+        bicycleVisuals.transform.localRotation = Quaternion.Euler(0, 0, -currentBicycleAngle);
+        handleBar.transform.localRotation = Quaternion.Euler(0,currentWheelAngle,0);
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles + new Vector3(
+                0,
+                currentWheelAngle*currentAccel, 
+                0
+            ) * Time.fixedDeltaTime
+        );
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ennemy")
+        {
+            Debug.Log("Should explode");
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        rb.isKinematic = true;
+        for (int i = 0; i < bicycleVisuals.transform.childCount; i++)
+        {
+            Transform child = bicycleVisuals.transform.GetChild(i);
+            Rigidbody childRb;
+            if (!child.TryGetComponent<Rigidbody>(out childRb))
+                continue;
+            float randX = Random.value - 0.5f;
+            float randY = Random.value - 0.5f;
+            float randZ = Random.value - 0.5f;
+            child.GetComponent<Collider>().isTrigger = false;
+            childRb.useGravity = true;
+            childRb.isKinematic = false;
+            childRb.AddForce(new Vector3(randX, randY, randZ).normalized * 500f);
+        }
+    }
+}
--- Assets/Scripts/Bicycle/Movement/BicycleMovement.cs
+++ Assets/Scripts/Bicycle/Movement/BicycleMovement.cs
@@ -12,6 +12,7 @@ public class BicycleMovement : MonoBehaviour
     [SerializeField] GameObject meshFront;
     [SerializeField] GameObject handleBreakFront;
     [SerializeField] GameObject meshRear;
+    [SerializeField] GameObject centerOfmassOBJ;
     Rigidbody ms_Rigidbody;
 
     float rbVelocityMagnitude;
@@ -23,20 +24,12 @@ public class BicycleMovement : MonoBehaviour
     {
         transform.rotation = Quaternion.identity;
         ms_Rigidbody = GetComponent<Rigidbody>();
-        ms_Rigidbody.mass = 400;
-        ms_Rigidbody.interpolation = RigidbodyInterpolation.Extrapolate;
-        ms_Rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
 
         //centerOfMass
-        GameObject centerOfmassOBJ = new GameObject("centerOfmass");
-        centerOfmassOBJ.transform.parent = transform;
-        centerOfmassOBJ.transform.localPosition = new Vector3(0.0f, -0.3f, 0.0f);
         ms_Rigidbody.centerOfMass = transform.InverseTransformPoint(centerOfmassOBJ.transform.position);
         //
-        BoxCollider collider = GetComponent<BoxCollider>();
-        collider.size = new Vector3(0.5f, 1.0f, 3.0f);
-
-        GenerateWheels();
+        //BoxCollider collider = GetComponent<BoxCollider>();
+        //collider.size = new Vector3(0.5f, 1.0f, 3.0f);
     }
 
     void OnEnable()
@@ -48,7 +41,6 @@ public class BicycleMovement : MonoBehaviour
     void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        /*
         verticalInput = Input.GetAxis("Vertical");
         medRPM = (frontWheel.rpm + rearWheel.rpm) / 2;
         rbVelocityMagnitude = ms_Rigidbody.velocity.magnitude;
@@ -64,16 +56,16 @@ public class BicycleMovement : MonoBehaviour
         }
 
         //steerAngle
-        float nextAngle = horizontalInput * 35.0f;
+        float nextAngle = horizontalInput * 45.0f;
         frontWheel.steerAngle = Mathf.Lerp(frontWheel.steerAngle, nextAngle, 0.125f);
 
 
-        if (Mathf.Abs(rearWheel.rpm) > 10000)
+        if (Mathf.Abs(rearWheel.rpm) > 50000)
         {
             rearWheel.motorTorque = 0.0f;
             rearWheel.brakeTorque = ms_Rigidbody.mass * 5;
         }
-        //
+
         if (rbVelocityMagnitude < 1.0f && Mathf.Abs(verticalInput) < 0.1f)
         {
             rearWheel.brakeTorque = frontWheel.brakeTorque = ms_Rigidbody.mass * 2.0f;
@@ -82,40 +74,23 @@ public class BicycleMovement : MonoBehaviour
         {
             rearWheel.brakeTorque = frontWheel.brakeTorque = 0.0f;
         }
-        //
-        */
+
         Stabilizer();
     }
 
     void Update()
     {
         //update wheel meshes
-        /*
         Vector3 temporaryVector;
         Quaternion temporaryQuaternion;
-        //
+        
         frontWheel.GetWorldPose(out temporaryVector, out temporaryQuaternion);
         meshFront.transform.position = temporaryVector;
-        meshHandleBreakFront.transform.rotation = new Quaternion(meshHandleBreakFront.transform.rotation.x, temporaryQuaternion.y, meshHandleBreakFront.transform.rotation.z, meshHandleBreakFront.transform.rotation.w);
-        //
+        meshFront.transform.rotation = temporaryQuaternion;
+        
         rearWheel.GetWorldPose(out temporaryVector, out temporaryQuaternion);
         meshRear.transform.position = temporaryVector;
         meshRear.transform.rotation = temporaryQuaternion;
-        */
-
-        float angle = 0;
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            angle = 25;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            angle = -25;
-        }
-
-        handleBreakFront.transform.rotation = Quaternion.Euler(0, angle, 0);
     }
 
     void Stabilizer()
@@ -131,40 +106,5 @@ public class BicycleMovement : MonoBehaviour
         {
             ms_Rigidbody.angularVelocity += new Vector3(0, horizontalInput * rpmSign, 0);
         }
-    }
-
-    void GenerateWheels()
-    {
-        // ------------------------------------------------------------------------------------
-        //settings
-        frontWheel.mass = rearWheel.mass = 40;
-        frontWheel.radius = rearWheel.radius = 0.5f;
-        frontWheel.wheelDampingRate = rearWheel.wheelDampingRate = 0.75f;
-        frontWheel.suspensionDistance = rearWheel.suspensionDistance = 0.35f;
-        frontWheel.forceAppPointDistance = rearWheel.forceAppPointDistance = 0;
-
-        //spring
-        JointSpring suspensionSpringg = new JointSpring();
-        suspensionSpringg.spring = 15000;
-        suspensionSpringg.damper = 4000;
-        suspensionSpringg.targetPosition = 0.5f;
-        frontWheel.suspensionSpring = rearWheel.suspensionSpring = suspensionSpringg;
-
-        //Friction
-        WheelFrictionCurve wheelFrictionCurveFW = new WheelFrictionCurve(); //friction FW
-        wheelFrictionCurveFW.extremumSlip = 2.0f;
-        wheelFrictionCurveFW.extremumValue = 4.0f;
-        wheelFrictionCurveFW.asymptoteSlip = 4.0f;
-        wheelFrictionCurveFW.asymptoteValue = 2.0f;
-        wheelFrictionCurveFW.stiffness = 2.0f;
-        frontWheel.forwardFriction = rearWheel.forwardFriction = wheelFrictionCurveFW;
-
-        WheelFrictionCurve wheelFrictionCurveSW = new WheelFrictionCurve(); //friction SW
-        wheelFrictionCurveSW.extremumSlip = 0.2f;
-        wheelFrictionCurveSW.extremumValue = 1.0f;
-        wheelFrictionCurveSW.asymptoteSlip = 0.5f;
-        wheelFrictionCurveSW.asymptoteValue = 0.75f;
-        wheelFrictionCurveSW.stiffness = 2.0f;
-        frontWheel.sidewaysFriction = rearWheel.sidewaysFriction = wheelFrictionCurveSW;
     }
 }
